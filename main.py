fe08f1755857cf2737279df04eb35bd371749ea5
animeswin <163599618+animeswin@users.noreply.github.com>
2024-12-18 15:31:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+def lancar_dados():
+    dado1 = random.randint(1, 6)
+    dado2 = random.randint(1, 6)
+    soma = dado1 + dado2
+    return soma
+
+resultado = lancar_dados()
+print(f"O resultado do lançamento dos dados é: {resultado}")
